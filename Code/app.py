d48a5ec5e0269b25d3b5a990481304ec6bfba1c0
Paul Fletes <paul.fletes@gmail.com>
2023-06-12 10:55:46 -0500

    removes corpus files from Code directory

--- Code/app.py
+++ Code/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 # TODO: Initialize your histogram, hash table, or markov chain here.
 # Any code placed here will run only once, when the server starts.
-histogram = Histogram('tgg.txt')
+histogram = Histogram('./data/tgg.txt')
 
 @app.route("/")
 def home():
