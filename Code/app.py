b1d6ccfa1654f57ce9305a76078a43aade1ac903
Paul Fletes <paul.fletes@gmail.com>
2024-01-31 19:59:33 -0600

    setup flask server to render word on page reload

--- Code/app.py
+++ Code/app.py
@@ -1,17 +1,22 @@
 """Main script, uses other modules to generate sentences."""
-from flask import Flask
+from flask import Flask, render_template
+from histogram import histogram, random_word, read_source_text
 
 
 app = Flask(__name__)
 
 # TODO: Initialize your histogram, hash table, or markov chain here.
 # Any code placed here will run only once, when the server starts.
-
+# Read source text and generate histogram
+source_text = read_source_text('data/source_text.txt')
+word_histogram = histogram(source_text)
 
 @app.route("/")
 def home():
     """Route that returns a web page containing the generated text."""
-    return "<p>TODO: Return a word here!</p>"
+    # Sample a random word from the histogram
+    sampled_word = random_word(word_histogram)
+    return render_template('index.html', word=sampled_word)
 
 
 if __name__ == "__main__":
